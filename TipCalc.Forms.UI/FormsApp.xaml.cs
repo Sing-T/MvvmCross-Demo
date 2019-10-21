6ae5dfba6d145a838328aee5e1f8340b588b2ac6
Sing-T <nemo.singt@gmail.com>
2019-10-22 00:27:37 +0800

    MvvmCross.Forms & IOS App

---
+++ TipCalc.Forms.UI/FormsApp.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TipCalc.Forms.UI
+{
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TipCalc.Forms.UI/Pages/TipView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmCross.Forms.Views;
+using TipCalc.Core.ViewModels;
+using Xamarin.Forms;
+
+namespace TipCalc.Forms.UI.Pages
+{
+    public partial class TipView : MvxContentPage<TipViewModel>
+    {
+        public TipView()
+        {
+            InitializeComponent();
+        }
+    }
+}
